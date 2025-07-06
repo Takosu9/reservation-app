bdbee3d3bb03e0cddfc9235b805817e6edee233d
Takosu0 <tago.takuhito@gmail.com>
2025-07-06 12:04:08 +0900

    navberページの途中まで

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { NavbarComponent } from '../common/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
